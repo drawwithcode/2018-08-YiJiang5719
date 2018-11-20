ff1edfe09ffc831d63b913d6daa06b03b033b9d2
Yijiang <Yijiang5719@gmail.com>
2018-11-21 00:52:23 +0100

    interaction via device

--- sketch.js
+++ sketch.js
@@ -1,11 +1,47 @@
-function preload(){
-  // put preload code here
+var value = 255;
+var imgLINK;
+var imgMIFA;
+var mySong;
+
+function preload() {
+  imgLINK = loadImage('assets/Link.png');
+  imgMIFA = loadImage('assets/MIFA.png');
+  mySong = loadSound('assets/KillerSoap - Distance copy.mp3');
 }
 
+
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+
+  // by defaults equals to 30
+  setShakeThreshold(30);
+  frameRate(12);
+  mySong.play();
+
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  //fill(255-value);
+  imageMode(CENTER);
+
+  tint(255, value);
+  image(imgMIFA,width/2,height/2,width,width);
+  tint(255, 255);
+  image(imgLINK,width/2,height/2,width,width);
+  textSize(24);
+  textAlign(CENTER);
+  text('via Shaking hands, memories will be gone',width/2,height-200)
+}
+
+function deviceShaken() {
+  value = value - 10;
+  if (value <= 0) {
+    value = 255;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
